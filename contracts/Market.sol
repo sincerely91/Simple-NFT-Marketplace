421a45352d2558ff015c8f8ad6a746cee45f21ec
blockchaindev91 <hisamitsuteru@gmail.com>
2022-01-06 23:01:57 +0530

    clean code

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
 
 contract Market is ReentrancyGuard {
