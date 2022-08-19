1ec03826d06cc62a0c27963d81e696557b1db87c
blockchaindev91 <hisamitsuteru@gmail.com>
2022-08-19 08:14:47 +0530

    changing gateway

--- contracts/ArtDodger.sol
+++ contracts/ArtDodger.sol
@@ -2,21 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ERC2981Royalties.sol";
 import "hardhat/console.sol";
 
-contract ArtDodger is
-    Ownable,
-    ERC721URIStorage,
-    ERC721Enumerable,
-    ERC2981Royalties
-{
+contract ArtDodger is Ownable, ERC721Enumerable, ERC2981Royalties {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    string public baseURI = "https://gateway.pinata.cloud/ipfs/";
+    mapping(uint256 => string) private _hashes;
+
+    function setBaseURI(string calldata _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+    function uri(uint256 tokenId) public view returns (string memory) {
+        return string(abi.encodePacked(baseURI, _hashes[tokenId]));
+    }
 
     constructor() ERC721("Art Dodger", "ARTD") {}
 
@@ -28,7 +31,7 @@ contract ArtDodger is
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI_);
+        _hashes[newItemId] = tokenURI_;
         if (royaltyPercent > 0) {
             _setTokenRoyalty(newItemId, royaltyRecipient, royaltyPercent);
         }
@@ -53,30 +56,27 @@ contract ArtDodger is
         address from,
         address to,
         uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
+    ) internal override(ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721URIStorage, ERC721)
+        override(ERC721)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
     }
 
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721URIStorage, ERC721)
-    {
+    function _burn(uint256 tokenId) internal override(ERC721) {
         super._burn(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, IERC165)
+        override(ERC721Enumerable, IERC165)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
